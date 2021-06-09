465e328c710d64e3140355325d0b9a192f290e3a
yuccie <>
2021-06-09 23:42:53 +0800

    完善

--- about-js/test-modules/moduleTwo.js
+++ about-js/test-modules/moduleTwo.js
@@ -1,6 +1,7 @@
 function sayHi(user) {
   alert(`Hello, ${user}!`);
 }
+module.exports = ccc;
 
 // 就像我们在函数之前添加了 "export default" 一样
 export {sayHi as default};
--- about-js/test-modules/modulesOne.js
+++ about-js/test-modules/modulesOne.js
@@ -1,3 +1,11 @@
 export function sayHi(str = 'hello') {
   console.log(str)
 }
+
+export let one = 'oned';
+
+setTimeout(() => one = 'two', 2000)
+
+setTimeout(() => console.log(one, '5s'), 5000)
+
+console.log(Object.getOwnPropertyDescriptors(one), 'aa', this)
